e26d69d7ce8b11cadbf141bd614f5f8373dc90de
lihuan <iijx@icloud.com>
2026-07-19 17:08:15 +0800

    update vite.config.js

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import { VitePWA } from "vite-plugin-pwa";
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/password-vision/",
+  base: "/pwd/",
   plugins: [
     vue(),
     tailwindcss(),
